9533a6ecbf6d0da54eef94bb6eafda104becbaa7
00be <66236573+00be@users.noreply.github.com>
2024-04-26 20:59:27 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+function scrollCarousel(myListCarousel, direction) {
+    const carousel = document.getElementById(myListCarousel);
+    const scrollAmount = 300; 
+
+    if (direction === 'left') {
+        carousel.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        carousel.scrollLeft += scrollAmount;
+    }
+}
+
+function scrollCarousel(top10Carousel, direction) {
+    const carousel = document.getElementById(top10Carousel);
+    const scrollAmount = 300; 
+    if (direction === 'left') {
+        carousel.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        carousel.scrollLeft += scrollAmount;
+    }
+}
+
+function scrollCarousel(estrangSeriesCarousel, direction) {
+    const carousel = document.getElementById(estrangSeriesCarousel);
+    const scrollAmount = 300; 
+    if (direction === 'left') {
+        carousel.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        carousel.scrollLeft += scrollAmount;
+    }
+}
+
+function scrollCarousel(humorCarousel, direction) {
+    const carousel = document.getElementById(humorCarousel);
+    const scrollAmount = 300;
+
+    if (direction === 'left') {
+        carousel.scrollLeft -= scrollAmount;
+    } else if (direction === 'right') {
+        carousel.scrollLeft += scrollAmount;
+    }
+}
+
+
